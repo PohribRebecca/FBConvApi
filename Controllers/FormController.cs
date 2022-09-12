8d4e975986e19de747b5f9ca6a8e48c02cbdbeb3
Pohrib Rebecca <PohribRebecca@users.noreply.github.com>
2022-09-12 12:24:21 +0300

    Add project files.

---
+++ Controllers/FormController.cs
@@ -0,0 +1,27 @@
+﻿using FBConvApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBConvApi.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public int FormAction(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return 1;
+            }
+            else
+                return 0;
+        }
+    }
+
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBConvApi.Models
+{
+    public class FormModel
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+    }
+}
